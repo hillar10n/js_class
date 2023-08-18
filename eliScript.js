c19f7e9cff982b2183ccc7fb98ad01e89db18328
Mayowa Oladimeji Oyinlola <lansoyin@gmail.com>
2023-08-18 15:03:28 +0100

    push
    another psuh

---
+++ eliScript.js
@@ -0,0 +1,53 @@
+// let greetings = "hi there!";
+
+// function greet() {
+// 	greetings;
+// 	let greeting = "hi, am diff!";
+// 	console.log(greetings);
+// 	console.log("inside function " + greeting);
+// }
+
+// function secondGreet() {
+// 	console.log(greeting);
+// }
+
+// secondGreet();
+
+// console.log("our main greet " + greetings);
+// greet();
+
+// function typeFood() {
+// 	return "i love rice so much";
+// }
+
+// typeFood();
+
+// function addTwoNums() {
+// 	let a = 5 * 5;
+// 	return a;
+// }
+
+// function test() {
+// 	let a = addTwoNums();
+// 	let b = 6;
+// 	let c = a + b;
+// 	return c;
+// }
+
+// test();
+
+// arrow functions
+
+// let greet2 = num => num * 5;
+
+// let manDown = (a = 3, b = 6) => {
+// 	return a + b;
+// };
+
+// let multiplyByTwo = (numberOne) => numberTwo * 2;
+
+// let game = function () {
+// 	console.log("am a function");
+// };
+
+// game();
